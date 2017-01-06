642683a3783270353a92c48687b9bc4d332be372
Diego J. Romero López <diegoj@rocinante.local>
2017-01-06 19:40:59 +0100

    Bootstrap and JQuery are included as static files in public dir

--- src/main/java/coursemanager/Application.java
+++ src/main/java/coursemanager/Application.java
@@ -3,13 +3,24 @@ package coursemanager;
 import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @SpringBootApplication
-public class Application {
+@EnableAutoConfiguration
+@ComponentScan
+public class Application extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
